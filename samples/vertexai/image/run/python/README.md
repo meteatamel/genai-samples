e6ce18e278f173959b602997d06043d8b22b19a8
Mete Atamel <atamel@google.com>
2023-08-04 15:55:23 +0100

    Sample: VertexAI imagegen, Cloud Run, Python #9

---
+++ samples/vertexai/image/run/python/README.md
@@ -0,0 +1,41 @@
+# VertexAI image generation, Cloud Run, Python
+
+A VertexAI image generation sample to run on Cloud Run with Python.
+
+First, change `PROJECT_ID` in [config.sh](config.sh) to your own.
+
+Run [setup.sh](setup.sh) to enable required services:
+
+```bash
+../setup.sh
+```
+
+Install dependencies:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+Run locally:
+
+```bash
+python3 main.py
+```
+
+Test locally:
+
+```bash
+curl http://localhost:8080
+```
+
+Deploy to cloud:
+
+```bash
+./deploy.sh
+```
+
+Test in the cloud:
+
+```sh
+./test_cloud.sh
+```
--- samples/vertexai/image/run/python/main.py
+++ samples/vertexai/image/run/python/main.py
@@ -1,11 +1,29 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
 import base64
 import google.auth
 import google.auth.transport.requests
 import json
 import os
 import requests
+
+from flask import Flask
+from google.cloud import storage
 from io import BytesIO
 from PIL import Image
+
+app = Flask(__name__)
 
 PROJECT_ID = "genai-atamel"
 LOCATION = "us-central1"
@@ -13,6 +31,8 @@ LOCATION = "us-central1"
 AI_PLATFORM_URL = f"https://{LOCATION}-aiplatform.googleapis.com"
 IMAGE_MODEL_NAME = "imagegeneration"
 PREDICT_URL = f"{AI_PLATFORM_URL}/v1/projects/{PROJECT_ID}/locations/{LOCATION}/publishers/google/models/{IMAGE_MODEL_NAME}:predict"
+
+BUCKET_NAME = "genai-atamel-images"
 
 def get_access_token():
     credentials, project_id = google.auth.default()
@@ -78,15 +98,32 @@ def generate_images(prompt, **kwargs):
             print(f"No predictions for prompt: {prompt}")
     return images
 
-def save_images(images, folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+def upload_images_to_gcs(images):
+    # TODO: Create the bucket if it doesn't exist
+    for idx, image in enumerate(images):
+        file_name = f"image_{idx}.png"
+        upload_image_to_gcs(image, file_name)
 
-    for idx, img in enumerate(images):
-        img_path = os.path.join(folder, f"image_{idx}.png")
-        img.save(img_path)
-        print(f"Saved {img_path}")
+def upload_image_to_gcs(image, file_name, bucket_name=BUCKET_NAME):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    bytes_io = BytesIO()
+    image.save(bytes_io, format='PNG')
+    blob.upload_from_file(bytes_io, rewind=True, content_type="image/png")
+
+    blob.make_public()
+    file_url = blob.public_url
+    print(f"Uploaded file: {file_name} to bucket: {bucket_name} with file url: {file_url}")
+    return file_url
+
+@app.route("/")
+def hello_world():
+    images = generate_images("happy dogs", sample_count=4)
+    upload_images_to_gcs(images)
+    return f"Created images {len(images)}!"
+
 
 if __name__ == "__main__":
-    images = generate_images("happy dogs", sample_count=2)
-    save_images(images, "images")
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
