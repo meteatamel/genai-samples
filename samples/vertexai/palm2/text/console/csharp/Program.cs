d430e90305ca1eeedf8aeae94065e1b7ef47ca83
Mete Atamel <atamel@google.com>
2023-10-16 13:35:50 +0100

    Minor edits

--- samples/vertexai/palm2/text/console/csharp/Program.cs
+++ samples/vertexai/palm2/text/console/csharp/Program.cs
@@ -1,49 +1,8 @@
-﻿using Google.Apis.Auth.OAuth2;
-using Google.Apis.Http;
-using Newtonsoft.Json;
-using System.Net.Http.Headers;
-using System.Text;
-
-// Adjust for your project
-const string PROJECT_ID = "genai-atamel";
-const string REGION = "us-central1";
-
-if (string.IsNullOrEmpty(REGION) || string.IsNullOrEmpty(PROJECT_ID))
+﻿public class Program
 {
-    throw new Exception("Environment variable 'REGION' or 'PROJECT_ID' not set.");
-}
-
-string apiUrl = $"https://{REGION}-aiplatform.googleapis.com/v1/projects/{PROJECT_ID}/locations/{REGION}/publishers/google/models/text-bison:predict";
-
-var payload = new
-{
-    instances = new[]
+    static async Task Main(string[] args)
     {
-        new
-        {
-            prompt = "Give me ten interview questions for the role of program manager."
-        }
-    },
-    parameters = new
-    {
-        temperature = 0.2,
-        maxOutputTokens = 256,
-        topK = 40,
-        topP = 0.95
+        var text = await Rest.GenerateText("Give me ten interview questions for the role of program manager.");
+        Console.WriteLine($"Response: {text}");
     }
-};
-
-GoogleCredential credential = GoogleCredential.GetApplicationDefault();
-//var token = await credential.UnderlyingCredential.GetAccessTokenForRequestAsync();
-var handler = credential.ToDelegatingHandler(new HttpClientHandler());
-using HttpClient httpClient = new(handler);
-//httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-HttpResponseMessage response = await httpClient.PostAsync(apiUrl,
-    new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
-
-string responseBody = await response.Content.ReadAsStringAsync();
-Console.WriteLine($"Response: {response.StatusCode}");
-Console.WriteLine(responseBody);
+}
---
+++ samples/vertexai/palm2/text/console/csharp/Rest.cs
@@ -0,0 +1,60 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+public class Rest
+{
+    // Adjust for your project
+    const string ProjectId = "genai-atamel";
+    const string Location = "us-central1";
+
+    const string AiPlatformUrl = $"https://{Location}-aiplatform.googleapis.com";
+    const string ModelName = "text-bison";
+    const string PredictUrl = $"{AiPlatformUrl}/v1/projects/{ProjectId}/locations/{Location}/publishers/google/models/{ModelName}:predict";
+
+    public async static Task<string> GenerateText(string prompt)
+    {
+        string payload = GeneratePayload(prompt);
+        string response = await SendRequest(payload);
+        return response;
+    }
+
+    private static string GeneratePayload(string prompt)
+    {
+        var payload = new
+        {
+            instances = new[]
+            {
+                new
+                {
+                    prompt = prompt
+                }
+            },
+            parameters = new
+            {
+                temperature = 0.2,
+                maxOutputTokens = 256,
+                topK = 40,
+                topP = 0.95
+            }
+        };
+        return JsonConvert.SerializeObject(payload);
+    }
+
+    private async static Task<string> SendRequest(string payload)
+    {
+        GoogleCredential credential = GoogleCredential.GetApplicationDefault();
+        var handler = credential.ToDelegatingHandler(new HttpClientHandler());
+        using HttpClient httpClient = new(handler);
+
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        HttpResponseMessage response = await httpClient.PostAsync(PredictUrl,
+            new StringContent(payload, Encoding.UTF8, "application/json"));
+
+        string content = await response.Content.ReadAsStringAsync();
+        return content;
+    }
+}
