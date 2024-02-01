1a436b67d4be8dca06332983a5dfa1008c47dc2d
Mete Atamel <atamel@google.com>
2024-02-01 13:03:29 +0000

    Samples: VertexAI Gemini Text/Image to Text, C# with VertexAI library

---
+++ vertexai/gemini/console/csharp/sdk/GenerateTextFromImageGcs.cs
@@ -0,0 +1,69 @@
+﻿using Google.Api.Gax.Grpc;
+using Google.Cloud.AIPlatform.V1;
+
+public class GenerateTextFromImageGcs
+{
+    const string ProjectId = "genai-atamel";
+    const string Location = "us-central1";
+    const string Publisher = "google";
+    const string Model = "gemini-pro-vision";
+
+    public async static Task Generate()
+    {
+        // Create client
+        var predictionServiceClient = new PredictionServiceClientBuilder
+        {
+            Endpoint = $"{Location}-aiplatform.googleapis.com"
+        }.Build();
+
+        // Prompt
+        string prompt = "Describe this image in detail";
+        string imageUrl = "gs://cloud-samples-data/generative-ai/image/320px-Felis_catus-cat_on_snow.jpg";
+        Console.WriteLine($"Prompt: {prompt}");
+        Console.WriteLine($"ImageUrl: {imageUrl}");
+
+        // Initialize request argument(s)
+        var content = new Content
+        {
+            Role = "USER"
+        };
+        content.Parts.AddRange(
+        [
+            new() {
+                Text = prompt
+            },
+            new() {
+                FileData = new() {
+                    MimeType = "image/png",
+                    FileUri = imageUrl
+                }
+            }
+        ]);
+
+        var generateContentRequest = new GenerateContentRequest
+        {
+            Model = $"projects/{ProjectId}/locations/{Location}/publishers/{Publisher}/models/{Model}",
+            GenerationConfig = new GenerationConfig
+            {
+                Temperature = 0.4f,
+                TopP = 1,
+                TopK = 32,
+                MaxOutputTokens = 2048
+            }
+        };
+        generateContentRequest.Contents.Add(content);
+
+        // Make the request, returning a streaming response
+        using PredictionServiceClient.StreamGenerateContentStream response = predictionServiceClient.StreamGenerateContent(generateContentRequest);
+
+        Console.Write($"Response: ");
+
+        // Read streaming responses from server until complete
+        AsyncResponseStream<GenerateContentResponse> responseStream = response.GetResponseStream();
+        while (await responseStream.MoveNextAsync())
+        {
+            GenerateContentResponse responseItem = responseStream.Current;
+            Console.WriteLine(responseItem.Candidates[0].Content.Parts[0].Text);
+        }
+    }
+}
